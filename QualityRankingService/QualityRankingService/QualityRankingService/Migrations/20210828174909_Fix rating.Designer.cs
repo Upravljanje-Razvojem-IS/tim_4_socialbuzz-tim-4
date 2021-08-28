7f6d7f49888fc014fe55720e47fa8a25f8101e66
TheJelena <jelena.vojnovic2812@gmail.com>
2021-08-28 19:50:02 +0200

    fix

--- QualityRankingService/QualityRankingService/QualityRankingService/Migrations/20210828174909_Fix rating.Designer.cs
+++ QualityRankingService/QualityRankingService/QualityRankingService/Migrations/20210828174909_Fix rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QualityRanking.Database;
 
 namespace QualityRanking.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20210828174909_Fix rating")]
+    partial class Fixrating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ QualityRankingService/QualityRankingService/QualityRankingService/Migrations/20210828174909_Fix rating.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace QualityRanking.Migrations
+{
+    public partial class Fixrating : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserId",
+                table: "Rankings",
+                newName: "RaterId");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "RateeId",
+                table: "Rankings",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RateeId",
+                table: "Rankings");
+
+            migrationBuilder.RenameColumn(
+                name: "RaterId",
+                table: "Rankings",
+                newName: "UserId");
+        }
+    }
+}
